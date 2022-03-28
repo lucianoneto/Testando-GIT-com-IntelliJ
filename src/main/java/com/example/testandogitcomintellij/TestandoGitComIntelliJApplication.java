25531e3dac132bec71aa3510234988aa4aa36169
Luciano Ferreira <LUCIANONETO1033@GMAIL.COM>
2022-03-28 09:11:58 -0300

    Primeiro commit

---
+++ src/main/java/com/example/testandogitcomintellij/TestandoGitComIntelliJApplication.java
@@ -0,0 +1,14 @@
+package com.example.testandogitcomintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestandoGitComIntelliJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestandoGitComIntelliJApplication.class, args);
+		System.out.println("Colocando o projeto do ItelliJ no GitHub");
+	}
+
+}
---
+++ src/test/java/com/example/testandogitcomintellij/TestandoGitComIntelliJApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testandogitcomintellij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestandoGitComIntelliJApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
